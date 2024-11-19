4f6453b7c333000a1a1737db54aa88915522a48a
mjcrntx <marjoycaranto29@gmail.com>
2024-11-19 18:33:37 +0800

    Pseudocode

---
+++ personal_info_collector.py
@@ -0,0 +1,11 @@
+#Use dictionary to store user's personal information
+
+#Use while loop to collect information
+
+#Add information in personal_info_collector.txt file
+
+#Print user information
+
+#Ask user if they want to add another personal information
+
+#Print all information if they answer "no"
