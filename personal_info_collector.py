d81796849cdb67740296a4f22637c0bfa59c287c
mjcrntx <marjoycaranto29@gmail.com>
2024-11-20 19:48:12 +0800

    Add loop and condition for age

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -9,15 +9,26 @@ while True:
             name = input("Name: ")
             
             if not re.match(r"^[A-Za-z\s'-.]+$", name): 
-                    print("Invalid name. Please try again.")
+                print("Invalid name. Please try again.")
             else: 
                 break
+        
+        while True:
+            age = int(input("Age: "))
             
-        age = int(input("Age: "))
+            if age >= 0:
+                break
+            else:
+                print("Age must be a non-negative number.")
+        
         birthday = input("Birthday: ")
-        sex = input("Sex: ")
+        
+        sex = input("Sex. Enter M if you are male, and F if you are female: ").upper()
+        
         phone_number = int(input("Phone Number: "))
+            
         address = input("Address: ")
+        
         email_account = input("Email account: ")
         
         personal_info[name] = {
@@ -30,23 +41,23 @@ while True:
             'email_account': email_account
         }
         
-        print(f"Your information: {personal_info[name]}")
+        #Print user information
+        print(f"Your personal information: {personal_info}")
         
+        #Ask user if they want to add another personal information
         add_new_info = input("Do you want to add new information? YES or NO: ").upper()
+        
+        if add_new_info == "YES":
+            continue
+        elif add_new_info == "NO":
+            break
+        else:
+            print("Invalid Input")
         
     except:
         print("Invalid Input")
     
-    if add_new_info == "YES":
-        continue
-    elif add_new_info == "NO":
-        break
-    else:
-        print("Invalid Input")
+    
 #Add information in personal_info_collector.txt file
-
-#Print user information
-print(personal_info)
-#Ask user if they want to add another personal information
 
 #Print all information if they answer "no"
