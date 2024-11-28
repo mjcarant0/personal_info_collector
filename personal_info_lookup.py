d012749f74d1e8ed3a3c66afada414adedcdfc93
mjcarant0 <marjoycaranto@gmail.com>
2024-11-29 06:21:09 +0800

    Pseudocode

---
+++ personal_info_lookup.py
@@ -0,0 +1,7 @@
+#Ask user to input their username
+
+#Ask user to input their password
+
+#If user's username and password in personal_info.txt, print their personal information
+
+#Ask user if they want to check other information
