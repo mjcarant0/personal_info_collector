026c42a5214e54ee35e88b9b04c9861b20e19307
mjcarant0 <marjoycaranto@gmail.com>
2024-11-29 14:29:55 +0800

    Add color to text

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -1,5 +1,7 @@
 import re #condition for name and email
 import os
+import colorama #adding color for text
+from colorama import Fore, Back, Style
 
 #File path for the .txt file
 os.chdir(r'D:\BSCpE\PLD\Individual\personal_info_collector') 
@@ -11,76 +13,76 @@ personal_info = {}
 while True:
     try:
         while True: #loop and condition for username
-            username = input("Enter your username (at least 5 characters): ")
+            username = input(f"{Fore.YELLOW}Enter your username (at least 5 characters): {Style.RESET_ALL}")
             
             if len(username) >= 5:
                 if username in personal_info:
-                    print("Username already exists. Please choose a different username.")
+                    print(f"{Back.RED}{Style.BRIGHT} Username already exists. Please choose a different username. {Style.RESET_ALL}")
                 else:
                     break
             else:
-                print("Username must be at least 5 characters. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT}Username must be at least 5 characters. Please try again. {Style.RESET_ALL}")
                 
         while True: #loop and condition for password
-            password = input("Password (at least 8 characters): ")
+            password = input(f"{Fore.YELLOW}Password (at least 8 characters): {Style.RESET_ALL}")
             
             if len(password) >= 8:
                 break
             else:
-                print("Password must be at least 8 characters. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT} Password must be at least 8 characters. Please try again. {Style.RESET_ALL}")
         
         while True: #loop and condition for name
-            name = input("Name: ")
+            name = input(f"{Fore.YELLOW}Name: {Style.RESET_ALL}")
             
             if not re.match(r"^[A-Za-z\s'-.]+$", name): 
-                print("Invalid name. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT} Invalid name. Please try again. {Style.RESET_ALL}")
             else: 
                 break
         
         while True: #loop and condition for age
-            age = int(input("Age: "))
+            age = int(input(f"{Fore.YELLOW}Age: {Style.RESET_ALL}"))
             
             if age >= 0:
                 break
             else:
-                print("Age must be a non-negative number.")
+                print(f"{Back.RED}{Style.BRIGHT} Age must be a non-negative number. {Style.RESET_ALL}")
         
         while True: #loop and condition for birthday
-            print("Birthday")
+            print(f"{Fore.YELLOW}Birthday{Style.RESET_ALL}")
             
             while True:
-                month = input("Month: ").capitalize()
+                month = input(f"{Fore.YELLOW}Month: {Style.RESET_ALL}").capitalize()
                 
                 valid_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
                 
                 if month in valid_months:
                     break
                 else:
-                    print("Invalid Month. Please try again.")
+                    print(f"{Back.RED}{Style.BRIGHT} Invalid Month. Please try again. {Style.RESET_ALL}")
             
             while True:
-                day = int(input("Day: "))
+                day = int(input(f"{Fore.YELLOW}Day: {Style.RESET_ALL}"))
                 
                 if day >= 1 and day <= 31:
                     break
                 else:
-                    print("Invalid day. Please try again.")
+                    print(f"{Back.RED}{Style.BRIGHT} Invalid day. Please try again. {Style.RESET_ALL}")
                     
             while True:
-                year = int(input("Year: "))
+                year = int(input(f"{Fore.YELLOW}Year: {Style.RESET_ALL}"))
                 
                 if year >= 1800 and year <= 2024:
                     break
                 else:
-                    print("Invalid Year. Please try again.")
+                    print(f"{Back.RED}{Style.BRIGHT} Invalid Year. Please try again. {Style.RESET_ALL}")
             
             birthday = f"{month} {day}, {year}"
             
-            print(f"Birthday: {birthday}")
+            print(f"{Fore.YELLOW}Birthday:{Style.RESET_ALL} {birthday}")
             break
         
         while True: #loop and condition for sex
-            sex = input("Sex. Enter M if you are male, and F if you are female: ").upper()
+            sex = input(f"{Fore.YELLOW}Sex. Enter M if you are male, and F if you are female: {Style.RESET_ALL}").upper()
             
             if sex == "M":
                 sex = "Male"
@@ -89,36 +91,36 @@ while True:
                 sex = "Female"
                 break
             else:
-                print("Invalid sex. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT} Invalid sex. Please try again. {Style.RESET_ALL}")
         
         while True: #loop and condition for address
-            print("Address")
+            print(f"{Fore.YELLOW}Address{Style.RESET_ALL}")
             
-            address_line = input("Address Line: ")
-            country = input("Country: ").capitalize()
+            address_line = input(f"{Fore.YELLOW}Address Line: {Style.RESET_ALL}")
+            country = input(f"{Fore.YELLOW}Country: {Style.RESET_ALL}").capitalize()
             
             address = f"{address_line}, {country}"
-            print(f"Address: {address}")
+            print(f"{Fore.YELLOW}Address: {Style.RESET_ALL}{address}")
             break
         
         while True: #loop and condition for email account    
-            email_account = input("Email account: ")
+            email_account = input(f"{Fore.YELLOW}Email account: {Style.RESET_ALL}")
             email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
             
             if re.match(email, email_account):
                 break
             else:
-                print("Invalid Email Account. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT} Invalid Email Account. Please try again. {Style.RESET_ALL}")
         
         while True: #loop and condition for marital status
-            marital_status = input("Marital Status (Single, Married, Divorced, Separated, or Widowed): ").capitalize()
+            marital_status = input(f"{Fore.YELLOW}Marital Status (Single, Married, Divorced, Separated, or Widowed): {Style.RESET_ALL}").capitalize()
 
             valid_status = ['Single', 'Married', 'Divorced', 'Separated', 'Widow']
             
             if marital_status in valid_status:
                 break
             else:
-                print("Invalid Marital Status. Please try again.")
+                print(f"{Back.RED}{Style.BRIGHT} Invalid Marital Status. Please try again. {Style.RESET_ALL}")
                 
         personal_info[username] = {
             'username': username,
@@ -134,7 +136,7 @@ while True:
         
         #Print user information
         print("-----------------------------------------------------------")
-        print("This is your personal information")
+        print(f"{Style.BRIGHT}{Back.CYAN} All collected personal information {Style.RESET_ALL}")
         print(f"Username: {personal_info[username]['username']}")
         print(f"Password: {personal_info[username]['password']}")
         print(f"Name: {personal_info[username]['name']}")
@@ -148,17 +150,17 @@ while True:
         print("-----------------------------------------------------------")
         
         #Ask user if they want to add another personal information
-        add_new_info = input("Do you want to add new information? YES or NO: ").upper()
+        add_new_info = input(f"{Fore.CYAN} Do you want to add new information? YES or NO: {Style.RESET_ALL}").upper()
         
         if add_new_info == "YES":
             continue
         elif add_new_info == "NO":
             break
         else:
-            print("Invalid Input")
+            print(f"{Back.RED}{Style.BRIGHT} Invalid Input {Style.RESET_ALL}")
         
     except:
-        print("Invalid Input")
+        print(f"{Back.RED}{Style.BRIGHT} Invalid Input {Style.RESET_ALL}")
     
     
 #Add information in personal_info.txt file
@@ -180,7 +182,7 @@ if add_new_info == "NO":
 
 #Print all information if they answer "no"
     print("-----------------------------------------------------------")
-    print("All collected personal information")
+    print(f"{Style.BRIGHT}{Back.CYAN} All collected personal information {Style.RESET_ALL}")
     for name, info in personal_info.items():
         print(f"Username: {personal_info[username]['username']}")
         print(f"Name: {info['name']}")
